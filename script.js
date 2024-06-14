00b267b3e95e212c30f79cff5b76528ca247d5ac
Denilson <129228213+Deunisso@users.noreply.github.com>
2024-06-13 21:15:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const correctWord = "tay"; // A palavra correta com 3 letras
+const correctKeys = ["Tay", "tay", "Tayara", "tayara"]; // Lista de palavras corretas
 const correctNumber = 3; // O número a ser exibido se a palavra estiver correta
 
 function checkWord() {
-    const wordInput = document.getElementById('wordInput').value.toLowerCase(); // Convertendo para minúsculas
+    const wordInput = document.getElementById('wordInput').value.trim(); // Obtém a entrada do usuário e remove espaços extras no início e no final
     const resultElement = document.getElementById('result');
     
-    if (wordInput === correctWord.toLowerCase()) { // Convertendo para minúsculas
+    // Verifica se a entrada do usuário corresponde a alguma das palavras corretas
+    if (correctKeys.includes(wordInput)) {
         resultElement.textContent = `Parabéns! O número é: ${correctNumber}`;
         resultElement.style.color = 'green';
     } else {
